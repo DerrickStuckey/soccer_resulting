57bbee5a94322b1c4bbaf3a1b5f3d5e3bae2540c
Derrick <dstuckey@lotame.com>
2020-07-27 20:19:10 -0400

    added basic extrapolation model and plot

--- team_quality_regression.R
+++ team_quality_regression.R
@@ -25,7 +25,8 @@ home.stats.2020 <- data.frame("Date"=game.stats.2020$Date,
                               "xG.Against"=game.stats.2020$AxGtTeam,
                               "Goals"=game.stats.2020$HG,
                               "Goals.Against"=game.stats.2020$AG,
-                              "Points"=game.stats.2020$HPtsActual)
+                              "Points"=game.stats.2020$HPtsActual,
+                              "Points.xG.Simul"=game.stats.2020$HxPtsExpected)
 away.stats.2020 <- data.frame("Date"=game.stats.2020$Date,
                               "Location"="Away",
                               "Team"=game.stats.2020$Away,
@@ -33,7 +34,8 @@ away.stats.2020 <- data.frame("Date"=game.stats.2020$Date,
                               "xG.Against"=game.stats.2020$HxGtTeam,
                               "Goals"=game.stats.2020$AG,
                               "Goals.Against"=game.stats.2020$HG,
-                              "Points"=game.stats.2020$APtsActual)
+                              "Points"=game.stats.2020$APtsActual,
+                              "Points.xG.Simul"=game.stats.2020$AxPtsExpected)
 
 full.stats.2020 <- bind_rows(home.stats.2020,away.stats.2020)
 dim(home.stats.2020)
@@ -62,8 +64,63 @@ full.stats.2020 <- full.stats.2020 %>%
 
 ## Test predictive performance
 # use each game number as a cutoff, measure predictive power of past performance for future
-# measure average performance over all teams using leave-one-out cross-validation
 # compare xG-based past performance metrics to actual goals
 
+gameday.cutoffs <- 1:30
 
+holdout.mean.error.base.cutoffs <- c()
+holdout.mean.error.xg.cutoffs <- c()
+# holdout.mean.error.combined.cutoffs <- c()
+
+for (gameday.cutoff in gameday.cutoffs) {
+  training.data <- full.stats.2020 %>% filter(Game.Number <= gameday.cutoff)
+  test.data <- full.stats.2020 %>% filter(Game.Number > gameday.cutoff)
+  
+  # a simple model which assumes points per game continue unchanged for each team
+  ppg.before <- training.data %>% group_by(Team) %>% summarise(ppg.before = mean(Points))
+  ppg.after <- test.data %>% group_by(Team) %>% summarise(ppg.after = mean(Points))
+  ppg.merged <- ppg.before %>% inner_join(ppg.after, by=c("Team"="Team"))
+  holdout.mean.error.base <- mean(abs(ppg.merged$ppg.after - ppg.merged$ppg.before))
+  holdout.mean.error.base.cutoffs <- c(holdout.mean.error.base.cutoffs, holdout.mean.error.base)
+  
+  # a model which assumes points per game based on xG simulation continue
+  # unchanged for each team
+  ppg.before.xg <- training.data %>% group_by(Team) %>% summarise(ppg.before.simul = mean(Points.xG.Simul))
+  ppg.after <- test.data %>% group_by(Team) %>% summarise(ppg.after = mean(Points))
+  ppg.merged.xg <- ppg.before.xg %>% inner_join(ppg.after, by=c("Team"="Team"))
+  holdout.mean.error.xg <- mean(abs(ppg.merged.xg$ppg.after - ppg.merged.xg$ppg.before.simul))
+  holdout.mean.error.xg.cutoffs <- c(holdout.mean.error.xg.cutoffs, holdout.mean.error.xg)
+  
+  # a model using a weighted combination of actual and xG-simulated points per game
+}
+
+basic.model.performance <- data.frame(
+  "GameDay"=gameday.cutoffs,
+  "Model"="Actual Results",
+  "Mean Error"=holdout.mean.error.base.cutoffs
+)
+xg.model.performance <- data.frame(
+  "GameDay"=gameday.cutoffs,
+  "Model"="xG-Simulated Results",
+  "Mean Error"=holdout.mean.error.xg.cutoffs
+)
+
+model.performance.comp <- bind_rows(basic.model.performance,
+                                    xg.model.performance)
+
+# plot Mean Error of extrapolating previous results forward
+# using actual results vs. xG-simulated results
+ggplot(data=model.performance.comp) + 
+  geom_line(mapping=aes(x=GameDay,y=Mean.Error,col=Model)) + 
+  scale_y_reverse()
+
+
+## measure average performance over all teams using leave-one-out cross-validation
+
+# teams <- unique(full.stats.2020$Team)
+
+# for (team in teams) {
+#   training.data <- full.stats.2020 %>% filter(Team != team)
+#   test.data <- full.stats.2020 %>% filter(Team == team)
+# }
 
